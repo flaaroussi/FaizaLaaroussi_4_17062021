38e08585c5db35464f2767b437506aea44fb47f7
faiza <faiza.laaroussi2016@gmail.com>
2021-06-26 23:39:39 +0200

    Version 1.11

--- modal.js
+++ modal.js
@@ -18,6 +18,8 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+//annuler le scroll du body lors de l'ouverture du formulaire.
+  document.body.style.overflow ="hidden";
 }
 //Fermer le formulaire = modal.
 const modalClose = document.querySelector(".close");
@@ -25,6 +27,8 @@ modalClose.addEventListener("click", doCloseModal);
 
 function doCloseModal() {
   modalbg.style.display = "none";
+//garder le scroll du body quand le formulaire est fermé.  
+  document.body.style.overflow ="auto";
 }
 
 
